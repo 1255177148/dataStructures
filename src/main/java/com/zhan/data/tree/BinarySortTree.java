18c7a1fa067a7cc6e93affabe7331807e72bae57
elvis <1255177148@qq.com>
2020-10-30 19:39:57 +0800

    一个二叉排序树的小demo(二)，关于删除二叉排序树的节点

--- src/main/java/com/zhan/data/tree/BinarySortTree.java
+++ src/main/java/com/zhan/data/tree/BinarySortTree.java
@@ -61,11 +61,45 @@ public class BinarySortTree {
     }
 
     /**
-     * 删除指定的节点
+     * <p>删除指定的节点,思路:</p>
+     * <p>先找到要删除的目标节点 targetNode 和父节点 parent, 这时 targetNode 会有以下三种情况</p>
+     * <p>一、删除叶子节点时:</p>
+     * <p>  1、确定 targetNode 是 parent 的左子节点还是右子节点;</p>
+     * <p>  2、如果是左子节点, 则 parent.left = null, 否则 parent.right = null</p>
+     * <p>二、删除只有一颗子树的节点时:</p>
+     * <p>  1、先找到要删除的目标节点 targetNode</p>
      * @param value 要删除的节点的值
      */
     public void remove(int value){
+        if (root == null){
+            return;
+        }
+        // 1、先找到要删除的节点
+        Node targetNode = search(value);
+        if (targetNode == null){ // 如果找不到要删除的节点
+            return;
+        }
+        // 2、如果目标节点是根节点，并且此树只有根节点这一个节点，那么直接删除根节点
+        if (root.left == null && root.right == null){
+            root = null;
+            return;
+        }
 
+        // 3、找到目标节点的根节点
+        Node parent = searchParent(value);
+        // 4、如果要删除的节点是叶子节点
+        if (targetNode.left == null && targetNode.right == null){
+            // 判断目标节点是父节点的左子节点还是右子节点
+            if (parent.left != null && parent.left.value == value){ // 如果是左子节点
+                parent.left = null;
+            } else { // 如果是右子节点
+                parent.right = null;
+            }
+        } else if (targetNode.left != null && targetNode.right != null){ // 5、如果目标节点有两颗子树
+
+        } else { // 6、如果目标节点有一颗子树
+
+        }
     }
 
     @Data
