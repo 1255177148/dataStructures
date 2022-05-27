7a713a13f0256f79693e98333132a559a00c0d8b
elvis.he <elvis.he@yunlsp.com>
2022-05-27 17:47:53 +0800

    红黑树(三)--插入自平衡，未完版

--- src/main/java/com/zhan/data/tree/RedBlackTree.java
+++ src/main/java/com/zhan/data/tree/RedBlackTree.java
@@ -34,11 +34,58 @@ public class RedBlackTree {
     }
 
     /**
-     * 添加完节点后，对整个红黑树进行调整以满足红黑树的规则
+     * <p>添加完节点后，对整个红黑树进行调整以满足红黑树的规则</p>
+     * <pre>
+     *     1、如果父亲节点和叔叔节点都是红色的，则将父亲和叔叔变为黑色，祖父变为红色，继续向上以祖父开始做调整
+     *     2、如果没有叔叔节点或者叔叔节点是黑色的，父亲节点是红色的
+     *        2.1、如果父亲在祖父的左侧
+     *          (1)、如果当前节点在父节点的右侧，对父节点进行左旋，这时，当前节点和父节点需要互换，因为旋转后，当前节点和父节点的位置会互换
+     *          (2)、将父节点设置为黑色，祖父设置为红色
+     *          (3)、对祖父节点进行右旋
+     *        2.2、如果父亲在祖父的右侧
+     *          (1)、如果当前节点在父节点的左侧，对父节点进行右旋，这是，当前节点和父节点需要互换
+     *          (2)、将父节点设置为黑色，祖父设置为红色
+     *          (3)、对祖父节点进行左旋
+     *     3、将根节点设置为黑色
+     *
+     * </pre>
      * @param node
      */
-    public void fixAfterInsert(Node node){
+    public void balanceInsert(Node node){
+        Node father, gFather;
+        // 父节点是红色的
+        while ((father = node.getParent()) != null && !father.isBlack){
+            gFather = father.getParent();
+            if (gFather.getLeft() == father){ // 父节点在祖父的左侧
+                Node uncle = gFather.getRight();
+                if (uncle != null && !uncle.isBlack){
+                    setBlack(father);
+                    setBlack(uncle);
+                    setRed(gFather);
+                    node = gFather;
+                    continue;
+                }
+                if (node == father.getRight()){
+                    leftRotate(father);// 对父节点进行左旋
+                    //todo 这里的对象互换需要验证，我觉得这里没有互换，需要进行深拷贝互换
+                    Node temp = node;
+                    node = father;
+                    father = temp;
+                }
+            }
+        }
+    }
 
+    private void exchange(Node node1, Node node2){
+
+    }
+
+    private void setBlack(Node node){
+        node.setBlack(true);
+    }
+
+    private void setRed(Node node){
+        node.setBlack(false);
     }
 
     /**
